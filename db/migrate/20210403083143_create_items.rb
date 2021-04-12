403ced984971be9494c52e01b7b60ef5d33f5e9b
1414287356 <blackace223@gmail.com>
2021-04-12 21:09:30 +0900

    マイグレーションファイル編集

--- db/migrate/20210403083143_create_items.rb
+++ db/migrate/20210403083143_create_items.rb
@@ -1,16 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string  :product_name,           null: false,
+      t.string  :product_name,           null: false
       t.text    :product_description,    null: false
       t.integer :product_category_id,    null: false
       t.integer :product_status_id,      null: false
-      t.integer :shipping_fee_burden_id, null:false
+      t.integer :shipping_fee_burden_id, null: false
       t.integer :shipping_area_id,       null: false
       t.integer :shipping_day_id,        null: false
       t.integer :price,                  null: false
       t.references :user,                foreign_key: true
-      
     end
   end
 end
