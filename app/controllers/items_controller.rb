159f1449f5b03abe58c30271a99cf62f8c0e5ce2
1414287356 <blackace223@gmail.com>
2021-04-18 17:51:02 +0900

    Revert "商品一覧表示機能_first"
    This reverts commit f16cd0fe78009ca80a912d79e164c5aff71e08a6.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,18 +7,6 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  def show
-    @item = Item.find(params[:id])
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def destroy
-    
-  end
-
   def create
     @item = Item.new(item_params)
     if @item.save
