a79df5d343e4d14a2db8df4359746c779fd14ec0
1414287356 <blackace223@gmail.com>
2021-04-19 08:07:48 +0900

    商品詳細表示機能_second

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+
   def index
     @items = Item.all.order("id DESC")
   end
 
   def show
     @item = Item.find(params[:id])
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-  end
-
-  def destroy
-
   end
 
   def new
