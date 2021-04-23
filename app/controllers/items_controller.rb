1475a78427dc455d7c228995805bd1474dc334fb
1414287356 <blackace223@gmail.com>
2021-04-23 09:38:29 +0900

    商品情報編集機能_second

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,13 @@
 class ItemsController < ApplicationController
+  before_action :set_item, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :move_to_index, except: [:index, :show, :create, :new]
 
   def index
     @items = Item.all.order("id DESC")
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
@@ -14,12 +15,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
+    if @item.update(item_params) 
       render :show
     else
       render :edit
@@ -37,8 +36,18 @@ class ItemsController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:product_name, :product_description, :product_category_id, :product_status_id,
                                  :shipping_fee_burden_id, :shipping_area_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
